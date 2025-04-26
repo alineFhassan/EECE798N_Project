a14e2ae56c9280aaa87e4aa1b19469c77fbdeadd
zainabsaad99 <zynab.ahmad.saad@gmail.com>
2025-04-26 12:40:14 +0300

    Fixing bug of displacement responsibilties and requirements

--- frontend/app.py
+++ frontend/app.py
@@ -1006,6 +1006,7 @@ def offered_job():
             dept_response.raise_for_status()
             department = dept_response.json().get('department', [])
             job['department_name'] = department['department_name']
+        print("jobs", jobs, flush=True)
         return render_template('offered_job.html', jobs=jobs, stats=stats)
     
     except Exception as e:
